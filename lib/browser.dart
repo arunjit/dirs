30e0e4a09e5500a5e6c12637167930cc6ce0e35c
Arunjit Singh <opensrc@ajsd.in>
2014-08-24 18:12:25 +0530

    this is shit

--- lib/browser.dart
+++ lib/browser.dart
@@ -1,22 +1,60 @@
-import 'package:polymer/polymer.dart';
+library dirs.browser;
+
+import 'package:core_elements/core_ajax_dart.dart';
 import 'package:core_elements/core_drawer_panel.dart';
+import 'package:core_elements/core_list_dart.dart';
+import 'package:dirs/util.dart';
+import 'package:polymer/polymer.dart';
 
 @CustomTag('dir-browser')
 class Browser extends PolymerElement {
-  Browser.created() : super.created();
 
-  @published String path;
-  CoreDrawerPanel _drawerPanel = null;
-
-  String get currentDir => path.substring(path.lastIndexOf("/") + 1);
-
-  toggleSidebar() {
-    drawerPanel.selected = drawerPanel.selected == 'drawer' ? 'main' : 'drawer';
+  Browser.created() : super.created() {
+    _xhr = $['xhr'] as CoreAjax;
+    _drawerPanel = $['drawerPanel']  as CoreDrawerPanel;
+    _list = $['list'] as CoreList;
   }
 
-  CoreDrawerPanel get drawerPanel {
-    if (_drawerPanel == null)
-      _drawerPanel = $['drawerPanel']  as CoreDrawerPanel;
-    return _drawerPanel;
+  @published String baseUrl;
+  @published String path;
+
+  @observable ObservableList items = new ObservableList();
+
+  CoreAjax _xhr = null;
+  CoreDrawerPanel _drawerPanel = null;
+  CoreList _list = null;
+
+  String get currentDir => firstNonEmpty(path.substring(path.lastIndexOf('/') + 1), '/');
+  String get currentUrl => baseUrl + path;
+
+  toggleSidebar() {
+    _drawerPanel.selected = _drawerPanel.selected == 'drawer' ? 'main' : 'drawer';
+  }
+
+  pathChanged(oldPath, newPath) {
+    _xhr.url = currentUrl;  // No dynamic binding :(.
+  }
+
+  update(_, json) {
+    json = json as Map;
+    items.clear();
+    (json['response'] as List).forEach((item) => items.add(new FileInfo.fromJson(item)));
+  }
+}
+
+class FileInfo {
+  bool isDir;
+  String name;
+  String path;
+
+  // View args
+  bool selected = false;
+
+  FileInfo({this.isDir, this.name, this.path});
+
+  FileInfo.fromJson(Map json) {
+    isDir = json['is_dir'];
+    name = json['name'];
+    path = json['path'];
   }
 }

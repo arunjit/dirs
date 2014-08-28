b3c70821a91bd776506b47ca2e6a91ed9345aa1d
Arunjit Singh <opensrc@ajsd.in>
2014-08-28 21:52:45 +0530

    dirs part working

--- src/components/browser.js
+++ src/components/browser.js
@@ -1,19 +1,56 @@
+function createRoute(path) {
+  return encodeURIComponent(path.substring(1));
+}
+function createPath(route) {
+  return '/' + decodeURIComponent(route);
+}
+function log() {
+  if (window.debug) {
+    console.log.apply(console, arguments);
+  }
+}
+
 Polymer('dir-browser', {
-  items: [],
+  dirs: [],
+  images: [],
+  currentDir: '',
+  route: '',
+  path: '',
   publish: {
-    baseUrl: '',
-    path: ''
+    base: '',
+    start: ''
   },
-  currentUrl: function() {
-    return this.baseUrl + this.path;
+  ready: function() {
+    log('dir-browser ready');
+    if (!this.route) {
+      this.path = this.start || '/';
+    } else {
+      this.path = createPath(this.route);
+    }
   },
-  currentDir: function() {
-    return this.path.substring(this.path.lastIndexOf('/') + 1) || '/';
+  pathChanged: function() {
+    log('pathChanged', this.path);
+    this.currentDir = this.path.substring(this.path.lastIndexOf('/') + 1) || '/';
+    this.$.xhr.url = this.base + this.path;
+    this.$.xhr.go();
+  },
+  routeChanged: function() {
+    log('routeChanged', this.route);
+    this.path = createPath(this.route);
   },
   toggleSidebar: function() {
     this.$.drawerPanel.togglePanel();
   },
-  update: function() {
-    console.log('Update');
+  update: function(event, http) {
+    var items = http.response.map(function(item) {
+      item.route = createRoute(item.path);
+      return item;
+    });
+    this.dirs = items.filter(function(item) {
+      return item.is_dir;
+    });
+    this.images = items.filter(function(item) {
+      return item.name.match(/\.(jpe?g|png|webp|gif|bmp)$/);
+    });
   }
 });
